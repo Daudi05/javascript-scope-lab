445a37fc793d441517a9c53ff8f86fc39143fb69
dwege <ojorodavid@gmail.com>
2025-11-28 02:15:26 +0300

    second commit

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 // Write your solution in this file!
-var burgers = ['Hamburger', 'Cheeseburger']
-var featuredDrink = 'Strawberry Milkshake'
+const burgers = ['Hamburger', 'Cheeseburger']
+let featuredDrink = 'Strawberry Milkshake'
 function addBurger(){
-    let newBurger = 'Flatburger'
+    const newBurger = 'Flatburger'
     burgers.push(newBurger)
 }
 if (true) {
- let anotherNewBurger = 'Maple Bacon Burger'
+ const anotherNewBurger = 'Maple Bacon Burger'
     burgers.push(anotherNewBurger)
 }
 addBurger()
