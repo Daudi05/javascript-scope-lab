d688472fd1fed9ee13f23fdeebdb29ddf206fec9
dwege <ojorodavid@gmail.com>
2025-11-28 02:04:03 +0300

    initial commit

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Write your solution in this file!
+var burgers = ['Hamburger', 'Cheeseburger']
+var featuredDrink = 'Strawberry Milkshake'
+function addBurger(){
+    let newBurger = 'Flatburger'
+    burgers.push(newBurger)
+}
+if (true) {
+ let anotherNewBurger = 'Maple Bacon Burger'
+    burgers.push(anotherNewBurger)
+}
+addBurger()
+function changeFeaturedDrink(){
+    featuredDrink = 'The Javashake'
+}
